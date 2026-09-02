081db8884573ef1c880b195925d67cf4cafaff28
Исламгулов Артур <artur.m.islamgulov@gazprombank.ru>
2026-09-02 14:52:56 +0300

    Initial Commit

---
+++ MockGen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MockGen
+//
+//  Created by AMIslamgulov on 02.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MockGen/MockGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockGenApp.swift
+//  MockGen
+//
+//  Created by AMIslamgulov on 02.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct MockGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
